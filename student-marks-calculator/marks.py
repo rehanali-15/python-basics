c48d99e9b04372e20ee9c5df7556a5b912b269f2
rehanali-15 <rehanali123r3@gmail.com>
2025-12-14 20:28:07 +0530

    Added student marks calculator mini project

---
+++ student-marks-calculator/marks.py
@@ -0,0 +1,12 @@
+name = input("Enter student name: ")
+
+m1 = int(input("Enter marks of subject 1: "))
+m2 = int(input("Enter marks of subject 2: "))
+m3 = int(input("Enter marks of subject 3: "))
+
+total = m1 + m2 + m3
+average = total / 3
+
+print("\nStudent Name:", name)
+print("Total Marks:", total)
+print("Average Marks:", average)
